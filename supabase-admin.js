aa75cd556db0aa5d91519784a0580d1b2aa79293
avpcea <286095842+avpcea@users.noreply.github.com>
2026-06-26 23:37:39 +0200

    Rename supabase variable to supabaseAdmin

--- supabase-admin.js
+++ supabase-admin.js
@@ -1,7 +1,7 @@
   <!-- SUPABASE UMD ADMINISTRACIÓN -->
   <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.45.4/dist/umd/supabase.js"></script>
   <script>
-    const supabase = window.supabase.createClient(
+    const supabaseAdmin = window.supabase.createClient(
       "https://hptnaoliiychgkxsaksy.supabase.co",
       "sb_publishable_yr41XhMZMxDXwyuJZRolVw_6o05FP8a"
     );
