22a7176d2dbac63e87cfbe0f24274c55a8bd47a6
avpcea <286095842+avpcea@users.noreply.github.com>
2026-07-07 19:13:17 +0200

    Delete supabase-admin.js

--- supabase-admin.js
+++
@@ -1,8 +0,0 @@
-  <!-- SUPABASE UMD ADMINISTRACIÓN -->
-  <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.45.4/dist/umd/supabase.js"></script>
-  <script>
-    const supabaseAdmin = window.supabase.createClient(
-      "https://hptnaoliiychgkxsaksy.supabase.co",
-      "sb_publishable_yr41XhMZMxDXwyuJZRolVw_6o05FP8a"
-    );
-  </script>
